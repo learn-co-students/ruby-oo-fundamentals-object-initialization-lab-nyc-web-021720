09da4112e81ff72f7ecb4329a55423d13d3ddceb
Tenacious-Q <mikequigley@quigleypublishing.com>
2020-02-20 09:26:29 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+
+class Dog
+    def initialize(name, breed = "Mutt")
+      @name = name 
+      @breed = breed 
+    end
+   
+    # I am baffled that the above code does not work 
+
+    # Note this lab was correct but was failing tests due to
+    # an issue with the terminal. 
+    # It was solved by closing and re-opening the text editor
+   
+end
